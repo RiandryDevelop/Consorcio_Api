a5303e417a9fd1a58d70b3babb5fd40c756708d9
RiandryDevelop <riandrydevs@gmail.com>
2025-01-22 17:22:40 -0400

    (Update): - Department Module
    - Refactoring Department Model/DTO
    - Migrating changes into the DB

--- Application/DTOs/DepartmentDTO.cs
+++ Application/DTOs/DepartmentDTO.cs
@@ -2,7 +2,7 @@
 {
     public class DepartmentDTO
     {
-        public int IdDepartment { get; set; }
+        public int DepartmentId { get; set; }
         public string? DepartmentName { get; set; }
         public string? CreationDate { get; set; }
         public string? UpdateDate { get; set; }
